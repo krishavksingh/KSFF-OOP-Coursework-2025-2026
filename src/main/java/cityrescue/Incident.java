93d609022f37da465f0b6e926f5fa9231b6113ca
krishavksingh <krishavksingh@gmail.com>
2026-03-01 15:04:33 +0000

    changed to use getters and setters, fields set to private/protected

--- src/main/java/cityrescue/Incident.java
+++ src/main/java/cityrescue/Incident.java
@@ -3,29 +3,67 @@ package cityrescue;
 import cityrescue.enums.*;
 
 public class Incident {
-    IncidentType type;
-    IncidentStatus status;
-    int severity;
-    int x;
-    int y;
-    int ID;
-   
+    private IncidentType type;
+    private IncidentStatus status;
+    private int severity;
+    private int x;
+    private int y;
+    private int id;
 
-    public Incident(IncidentType type2, int severity, int _x, int _y, int incidentId) {
-        type = type2;
+    public Incident(IncidentType _type, int _severity, int _x, int _y, int _id) {
+        type = _type;
         status = IncidentStatus.REPORTED;
+        severity = _severity;
         x = _x;
         y = _y;
-        ID = incidentId;
-    
-
-
-
+        id = _id;
     }
 
-    public IncidentStatus getStatus()
-    {
+    public IncidentType getType() {
+        return type;
+    }
+
+    public IncidentStatus getStatus() {
         return status;
     }
-    
+
+    public int getSeverity() {
+        return severity;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setType(IncidentType _type) {
+        type = _type;
+    }
+
+    public void setStatus(IncidentStatus _status) {
+        status = _status;
+    }
+
+    public void setSeverity(int _severity) {
+        severity = _severity;
+    }
+
+    public void setX(int _x) {
+        x = _x;
+    }
+
+    public void setY(int _y) {
+        y = _y;
+    }
+
+    public void setId(int _id) {
+        id = _id;
+    }
 }
--- src/main/java/cityrescue/Unit.java
+++ src/main/java/cityrescue/Unit.java
@@ -14,12 +14,8 @@ public class Unit {
     protected int y_dest;
     protected int worktick;
 
-    public UnitStatus getStatus()
-    {
-        return status;
-    }
-    public Unit(int stationId, int unitId, int _x, int _y)
-    {
+    // Constructor
+    public Unit(int stationId, int unitId, int _x, int _y) {
         status = UnitStatus.IDLE;
         stationID = stationId;
         unitID = unitId;
@@ -29,5 +25,85 @@ public class Unit {
         y_dest = _y;
         incidentId = -1;
     }
-    
+
+    public UnitType getType() {
+        return type;
+    }
+
+    public UnitStatus getStatus() {
+        return status;
+    }
+
+    public int getStationID() {
+        return stationID;
+    }
+
+    public int getUnitID() {
+        return unitID;
+    }
+
+    public int getIncidentId() {
+        return incidentId;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX_dest() {
+        return x_dest;
+    }
+
+    public int getY_dest() {
+        return y_dest;
+    }
+
+    public int getWorktick() {
+        return worktick;
+    }
+
+   
+    public void setType(UnitType type) {
+        this.type = type;
+    }
+
+    public void setStatus(UnitStatus status) {
+        this.status = status;
+    }
+
+    public void setStationID(int stationID) {
+        this.stationID = stationID;
+    }
+
+    public void setUnitID(int unitID) {
+        this.unitID = unitID;
+    }
+
+    public void setIncidentId(int incidentId) {
+        this.incidentId = incidentId;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setX_dest(int x_dest) {
+        this.x_dest = x_dest;
+    }
+
+    public void setY_dest(int y_dest) {
+        this.y_dest = y_dest;
+    }
+
+    public void setWorktick(int worktick) {
+        this.worktick = worktick;
+    }
 }
