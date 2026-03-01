a001690b06106b3ab38e91ba77cd0d57a5f79ed1
krishavksingh <krishavksingh@gmail.com>
2026-03-01 13:44:08 +0000

    added incident constructor

--- src/main/java/cityrescue/Incident.java
+++ src/main/java/cityrescue/Incident.java
@@ -5,6 +5,21 @@ import cityrescue.enums.*;
 public class Incident {
     IncidentType type;
     IncidentStatus status;
+    int severity;
+    int x;
+    int y;
+    int ID;
+
+    public Incident(IncidentType type2, int severity, int _x, int _y, int incidentId) {
+        type = type2;
+        status = IncidentStatus.REPORTED;
+        x = _x;
+        y = _y;
+        ID = incidentId;
+
+
+
+    }
 
     public IncidentStatus getStatus()
     {
